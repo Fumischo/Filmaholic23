b9aa4dcdfaa393a04d2d71f9c897f3260edfe001
fumischo <nil.admirari1009@gmail.com>
2019-12-23 17:01:53 +0900

    mofify some codes for view files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( application.css )
-Rails.application.config.assets.precompile += %w( separate_css/movie_show.css )
+# Rails.application.config.assets.precompile += %w( separate_css/movie_show.css )
+Rails.application.config.assets.precompile += %w( separate_css/* )
+Rails.application.config.assets.precompile += %w( test.js )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
