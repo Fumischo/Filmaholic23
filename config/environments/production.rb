d5bc46793e0cbee075e4a1876c2108fd0bb74011
fumischo <nil.admirari1009@gmail.com>
2019-12-26 12:35:16 +0900

    heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-end
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
