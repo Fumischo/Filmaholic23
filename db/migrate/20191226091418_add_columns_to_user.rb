53ecd69877d8f0007c612b45cff6ab6c9e0b6fc4
fumischo <nil.admirari1009@gmail.com>
2019-12-26 21:08:08 +0900

    heroku deploy

--- db/migrate/20191226091418_add_columns_to_user.rb
+++ db/migrate/20191226091418_add_columns_to_user.rb
@@ -5,6 +5,7 @@ class AddColumnsToUser < ActiveRecord::Migration[5.2]
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
     add_index :users, :confirmation_token, unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
 
