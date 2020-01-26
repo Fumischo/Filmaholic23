2476dcb82ddd6a25789088626f2a19f3f47ea6fb
fumischo <nil.admirari1009@gmail.com>
2020-01-26 23:03:26 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # root :to => 'oauth_test#index'
 
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    omniauth_callbacks: "users/omniauth_callbacks"
+  registrations: "users/registrations",
+  omniauth_callbacks: "users/omniauth_callbacks"
   }
 
 
